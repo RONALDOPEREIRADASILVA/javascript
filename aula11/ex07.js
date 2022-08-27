dcb8b9f719938f3b97c1b9b2de1c165ce4be3080
Ronaldo Pereira Da Silva <105816779+RONALDOPEREIRADASILVA@users.noreply.github.com>
2022-08-27 20:04:35 -0300

    CRIEI EX 09

---
+++ aula11/ex07.js
@@ -0,0 +1,7 @@
+var vel = 78
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel >60){//condição simples!
+    console.log(`você ultrapassou a velocidade permitida. Multado!`)
+
+}
+console.log(`dirija sempre usando cinto de segurança!`)
