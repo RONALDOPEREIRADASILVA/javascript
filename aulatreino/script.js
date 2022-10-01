006f3699ce38a1fbef52f6b6220829fce22c9a80
RONALDOPEREIRADASILVA <eletrotecnica.ronaldo@gmail.com>
2022-10-01 18:57:43 -0300

    aula de treino

--- aulatreino/script.js
+++ aulatreino/script.js
@@ -1,1 +1,13 @@
-alert("ola")
+function dados (){
+    var n1 = document.getElementById("txtn1")
+    var n2 = document.getElementById("txtn2")
+    var cartão = document.getElementById("txtn3")
+    var boletos = document.getElementById("txtn4")
+    var saidas = document.getElementById("txtn5")
+    var saldo = document.getElementById("saldo")
+    var gastos = document.getElementById("gastos")
+    var dados []
+    
+
+    
+}
